09bfcbe9c837fa03b754089cbdd55a5ae6b0fce2
michael posada <michaelposada@mail.adelphi.edu>
2019-11-25 19:56:59 -0500

    Created a funcitoning admin page, and able to add and resovle tickets

--- Scripts/addTicket.php
+++ Scripts/addTicket.php
@@ -28,13 +28,26 @@
 			}
 
 
-			if($boolean2==TRUE)
+			if($boolean2==True)
 			{
 				echo "Here";
 				$st = $dbh->prepare("UPDATE tickets SET category=? WHERE(userid=? AND ticketid=?)");
 	                        $st->bindParam(1, $catergory);
 	                        $st->bindParam(2, $userid);
 				$st->bindParam(3, $ticketid);
+				$st->execute();
+				if($result)
+                                {
+                                       $succ = false;
+                                       echo "Error";
+                                        //header('Location: ../index.html');
+                                       //figure out where we want to go
+                                }
+                                else
+                                {
+                                        $succ = true;
+                                        header('Location: ticketView.php');
+                                }
 
 			}
 			elseif($boolean2==FALSE)
@@ -112,6 +125,104 @@
 			{
 				$succ = true;
 				header('Location: ticketView.php');
+			}
+		}
+		if(array_key_exists('editTicket2',$_POST))
+		{
+			$input = array(100,200,300,400,500);
+                        $rand_keys = array_rand($input,1);
+                        $adminid = $input[$rand_keys];
+			$userid = $_SESSION['userid'];
+           		$catergory = $_POST['category'];
+            		$subject = $_POST['subject'];
+            		$description = $_POST['ticketdescription'];
+			$ticketid = $_POST['ticketid'];
+            		$status = $_POST['status'];
+			try
+                        {
+                                $dbh = new PDO('pgsql:dbname=postgres');
+                                echo "Connected to Database";
+                        } catch(PDOException $e){
+                                print "Error with Database: ".$e->getMessage()."<br/>";
+                                die();
+                        }
+			$boolean1 = False;
+			$boolean2 = False;
+			
+			if($status == "")
+			{
+				$boolean = True;
+			}
+			if($description == "")
+			{
+				$boolean2 = True;
+			}
+			if($boolean2 && $boolean1)
+			{
+				echo "Here";
+				$st = $dbh->prepare("UPDATE tickets SET category=? WHERE(adminid=? AND ticketid=?)");
+	                        $st->bindParam(1, $catergory);
+	                        $st->bindParam(2, $userid);
+				$st->bindParam(3, $ticketid);
+				$st->execute();
+				if($result)
+                                {
+                                       $succ = false;
+                                       echo "Error";
+                                        //header('Location: ../index.html');
+                                       //figure out where we want to go
+                                }
+                                else
+                                {
+                                        $succ = true;
+                                        header('Location: ticketView.php');
+                                }
+
+
+
+			}
+			elseif(!$boolean2 && !$boolean1)
+			{
+				if($status == "Fixed")
+				{
+					$x = date_create();
+                        		$day = date_format($x,"m/d/Y");
+                        		$day = strval($day);
+
+					$st = $dbh->prepare("UPDATE tickets SET category=?, description=?, status=?, dataresolved=?  WHERE(adminid=? AND ticketid=?)");
+					$st->bindParam(1, $catergory);
+					$st->bindParam(2, $description);
+					$st->bindParam(3, $status);
+					$st->bindParam(4, $day);
+					$st->bindParam(5, $userid);
+	                                $st->bindParam(6, $ticketid);
+					$st->execute();
+	                      	        $result = $st->fetch();
+				}
+				else
+				{
+					$st = $dbh->prepare("UPDATE tickets SET category=?, description=?, status=? WHERE(adminid=? AND ticketid=?)");
+                                        $st->bindParam(1, $catergory);
+                                        $st->bindParam(2, $description);
+                                        $st->bindParam(3, $status);
+                                        $st->bindParam(4, $userid);
+                                        $st->bindParam(5, $ticketid);
+                                        $st->execute();
+                                        $result = $st->fetch();
+                                }
+				echo "Right before if";
+	                        if($result)
+         	                {
+                 	              $succ = false;
+                 	               echo "Error";
+                        	        //header('Location: ../index.html');
+                 	               //figure out where we want to go
+                        	}
+                        	else
+                        	{
+                        	        $succ = true;
+                        	        header('Location: adminTicketPage.php');
+                        	}
 			}
 		}
 ?>
---
+++ Scripts/adminTicketPage.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en" class="login-register-bg"> 
+    <title>Create Ticket</title>
+        <head>
+            <link rel="stylesheet" type="text/css" href="../Assests/stylesheet.css">
+            <link href="https://fonts.googleapis.com/css?family=Biryani:800" rel="stylesheet">
+				<style>
+					body {
+						background-image: url("main.jpg");
+						background-repeat:no-repeat;
+						background-position: fixed;
+						background-size:cover;
+						zoom:120%;
+					}	
+					
+					hr{
+						overflow: visible; 
+						height: 30px;
+						border-style: solid;
+						border-color: black;
+						border-width: 1px 0 0 0;
+						border-radius: 20px;
+					}
+					hr:before { 
+						display: block;
+						content: "";
+						height: 30px;
+						margin-top: -31px;
+						border-style: solid;
+						border-color: black;
+						border-width: 0 0 1px 0;
+						border-radius: 20px;
+					}
+					
+					#wrapper {
+						margin-left:auto;
+						margin-right:auto;
+						width:960px;
+					}
+					
+					.column {
+					  float: left;
+					  width: 50%;
+					}
+					/* Clear floats after the columns */
+					.row:after {
+					  content: "";
+					  display: table;
+					  clear: both;
+					}
+					
+					/* vertical line separating columns */
+					.vl {
+					  border-left: 2px solid black;
+					  height: 650px;
+					  position: absolute;
+					  left: 50%;
+					  margin-left: -3px;
+					  top: 50;
+					}
+					
+					/* table borders */
+					table, th, td {
+						border: 1px solid black;
+					}
+					
+					
+					
+					/* dropdown css */
+					/* Style The Dropdown Button */
+					.dropbtn {
+					  background-color: #A9A9A9;
+					  color: black;
+					  padding: 2px;
+					  font-size: 16px;
+					  border: none;
+					  cursor: pointer;
+					}
+					/* The container <div> - needed to position the dropdown content */
+					.dropdown {
+					  position: relative;
+					  display: inline-block;
+					}
+					/* Dropdown Content (Hidden by Default) */
+					.dropdown-content {
+					  display: none;
+					  position: absolute;
+					  background-color: #f9f9f9;
+					  min-width: 160px;
+					  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+					  z-index: 1;
+					}
+					/* Links inside the dropdown */
+					.dropdown-content a {
+					  color: black;
+					  padding: 6px 6px;
+					  text-decoration: none;
+					  display: block;
+					}
+					/* Change color of dropdown links on hover */
+					.dropdown-content a:hover {background-color: #A9A9A9}
+					/* Show the dropdown menu on hover */
+					.dropdown:hover .dropdown-content {
+					  display: block;
+					}
+					/* Change the background color of the dropdown button when the dropdown content is shown */
+					.dropdown:hover .dropbtn {
+					  background-color: #A9A9A9;
+					 /* --------------------------- */
+					 /* search bar for ticket pages */
+					 /* --------------------------- */
+				</style>
+        </head>
+	<script>
+        function r2()
+        {
+		var tbl = document.getElementById("originalTable");
+		tbl.sytle.display = "none";
+        }
+       </script>
+		<body>		
+            <div id="view">
+				<div id="login">
+						<p style = "font-family:georgia,garamond,serif;font-size:16px;font-style:italic;">
+							<center>
+									<?php
+									session_start();
+									//Connect to DB
+									$dbh = pg_connect(" dbname=postgres");
+									if (!$dbh)
+									{
+										echo "An error occurred.\n";
+									        exit;
+									}
+										$userid = $_SESSION['userid'];
+									$result = pg_query($dbh, "SELECT username FROM adminm WHERE adminid = " .$userid.  "");
+								?>
+								<h1 class="title">Welcome <?php $r = pg_fetch_row($result); echo "$r[0].";?></h1>
+								<hr>
+								<div class="row">
+									<div class="column">
+										<table style="width:70%">
+										<h2 class="title" style = "font-size:20px">Previous Tickets.</h2> 
+										<form method="post">
+											Tag:
+											<select name="searchTag">
+											<option value="category">Category</option>
+											<option value="description">Description</option>
+											<option value="datacreated">Date Created</option>
+											<option value="status">Status</option>
+											<option value="adminid">Admin ID</option>
+											<option value="dataresolved">Date Resolved</option>
+											</select>
+											<input style="width: 300px;" type="text" name="search" class="ticketsearch" placeholder="Search..." required>
+											<input type="submit" value="&#x1F50D" name="submitSearch" onClick="r2()"> <br> <br>
+										</form>
+										<table id="searchTable">
+                                                                                <?php
+											if(array_key_exists('submitSearch',$_POST))
+											{
+												session_start();
+												//Connect to DB
+												$dbh = pg_connect(" dbname=postgres");
+												if (!$dbh)
+												{
+													echo "An error occurred With Connecting to the Database.\n";
+													exit;
+												}
+												$userid = $_SESSION['userid'];
+												$tag = $_POST['searchTag'];
+												$search = $_POST['search'];
+												//$result = pg_query($dbh, "SELECT * FROM tickets WHERE(userid='1' AND category='link1')");
+												$result = pg_query($dbh, "SELECT * FROM tickets WHERE(adminid ='".$userid."' AND ".$tag." = '".$search."')");
+												$i = pg_num_fields($result);
+												if ($result == false)
+												{
+													echo "An error occurred.\n";
+													exit;
+												}
+												$header1 = true;
+												while ($row = pg_fetch_row($result))
+												{
+													$count = count($row);
+													if($header1)
+													{
+														for($x = 0; $x < $count; $x++)
+														{
+															$columnName = pg_field_name($result, $x);
+															?>
+															<th><?php echo "$columnName"; ?></th>
+															<?php
+															if($x==$count-1)
+															{
+																?></tr><?php
+															}
+															?>
+															<?php
+															}
+													}
+													$header1 = false;
+													for($x = 0; $x < $count; $x++)
+													{
+														?>
+														<td><?php echo "$row[$x]"; ?></td>
+														<?php
+														if($x==$count-1)
+														{ 
+															?></tr><?php
+														}
+													}
+												}
+											}
+										?>
+										<table id="originalTable">
+										<?php
+									                session_start();
+                									//Connect to DB
+                									$dbh = pg_connect(" dbname=postgres");
+               										 if (!$dbh)
+               										 {
+                         									echo "An error occurred.\n";
+                        									exit;
+                									 }
+                									$userid = $_SESSION['userid'];
+                									$result = pg_query($dbh, "SELECT * FROM tickets WHERE adminid = " .$userid.  "");
+                									$i = pg_num_fields($result);
+                									if (!$result)
+                									{
+                        									echo "An error occurred.\n";
+                       										exit;
+               										}
+											$header1 = true;
+               										while ($row = pg_fetch_row($result))
+                									{
+                        									$count = count($row);
+												if($header1)
+												{
+													for($x = 0; $x < $count; $x++)
+                        										{
+														$columnName = pg_field_name($result, $x);
+														?>
+														<th><?php echo "$columnName"; ?></th>
+														<?php
+														if($x==$count-1)
+														{
+															?></tr><?php
+														}
+														?>
+													<?php
+													}
+													?>
+												<?php
+                        									}
+												$header1 = false;
+												for($x = 0; $x < $count; $x++)
+												{
+													?>
+													<td><?php echo "$row[$x]"; ?></td>
+													<?php
+													if($x==$count-1)
+                                                                                                        { 
+                                                                                                                ?></tr><?php
+                                                                                                        }
+                                                                                                    
+												}
+                									}
+										?>
+										</table>
+									</div>
+								
+									<div class="vl"></div>
+									
+									<div class="column"> 
+										<h3 class="title" style = "font-size:20px">Create Ticket.</h3> 
+										<div align="left" style="padding-left: 100px">
+										<form action="addTicket.php" method="post">
+											Subject:		 
+											<input type="text" name="subject" class="ticketsubject" placeholder="Enter ticket subject" required><br><br>
+											Category:		 
+											  <select name="category">
+												<option value="Error with Login">Error with Login</option>
+												<option value="Connection Error">Connection Error</option>
+												<option value="Other">Other</option>
+											  </select>
+											<br> <br>
+											Description:
+										</div>
+											<textarea type="text" name="ticketdescription" placeholder="Enter ticket description" class="form-control" rows="6" cols="75" required> </textarea>
+											<br> <br>
+											<input type="submit" name="addTicket" formaction="addTicket.php" value="Confirm ticket">
+										</form>
+									<br> <br> <br>
+									
+									----------------------------------------------------------------------------------------------------------------
+	<h3 class="title" style = "font-size:20px">Resolve Ticket.</h3> 
+										<div align="left" style="padding-left: 100px">
+										<form action="addTicket.php" method="post">
+
+											Subject:
+											<input type="text" name="subject" class="ticketsubject" placeholder="Enter ticket subject" required><br><br>
+											Category:
+											  <select name="category">
+													<option value="Error with Login">Error with Login</option>
+													<option value="Connection Error">Connection Error</option>
+													<option value="Other">Other</option>
+											  </select>
+											Status:
+											  <select name="status">
+													<option value="Fixed">Fixed</option>
+													<option value="Not Fixed">Not Fixed</option>
+											  </select>
+											<br> <br> 
+											Ticket ID:
+											<input type="text" name="ticketid" class="ticketsubject" placeholder="Enter Ticket ID" required><br><br>
+											Description:	
+										</div>	
+											<textarea type="text" name="ticketdescription" placeholder="Enter ticket description" class="form-control" rows="6" cols="75"> </textarea>
+											<br> <br>                                                               
+											<input type="submit" name="editTicket2" formaction="addTicket.php" value="Confirm edit">							
+										</form>
+									</div>
+							</center> 
+						</p>
+					</form>
+				</div>	
+            </div>
+		</body>
+</html>
